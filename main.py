97b876b532d75608b0c066b51e260dc228d91823
alayden07 <alaydine1@gmail.com>
2022-04-17 17:23:16 +0100

    test

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from collections import Counter
 import csv
 from datetime import datetime
 import pytz
+import matplotlib.pyplot as plt
 
 ###extraction des fichiers GZ###
 
@@ -64,72 +65,60 @@ with open('C:/Users/USER/Desktop/DS3/PFE/DataSet/txtFinal.log', 'w') as outfile:
         outfile.write("\n")
 
 
-# ###extraction liste des adresses IP###
-# def reader(filename):
+###extraction liste des adresses IP###
+def reader(filename):
+    with open(filename) as f:
+        log = f.read()
 
-#     with open(filename)  as f:
-#         log = f.read()
+        regexp = r'\d{1,}\.\d{1,}\.\d{1,}\.\d{1,}'  # r pour lire les lignes telles qu'elles sont :(tq les tabs ou les nouvelles lignes)
 
-#         regexp = r'\d{1,}\.\d{1,}\.\d{1,}\.\d{1,}'# r pour lire les lignes telles qu'elles sont :(tq les tabs ou les nouvelles lignes)
-
-#         ips_list = re.findall(regexp, log)
-#         return ips_list
-
-# reader('C:/Users/USER/Desktop/DS3/PFE/DataSet/txtFinal.txt')
+        ips_list = re.findall(regexp, log)
+        return ips_list
 
 
-# ###creer un fichier csv qui contient les adresses IP selon leurs frequences###
-# def count(ips_list):
-#     return(Counter(ips_list))
+reader('C:/Users/USER/Desktop/DS3/PFE/DataSet/txtFinal.log')
 
 
-# def write_csv(counter):
-#     with open('C:/Users/USER/Desktop/DS3/PFE/DataSet/output.csv','w') as csvfile:
-#         writer = csv.writer(csvfile)
-#         header = ['IP' , 'FREQUENCY']
-#         writer.writerow(header)
+###creer un fichier csv qui contient les adresses IP selon leurs frequences###
+def count(ips_list):
+    return (Counter(ips_list))
 
-#         for item in counter:
-#             writer.writerow( (item, counter[item]) )
 
-# write_csv(count(reader('C:/Users/USER/Desktop/DS3/PFE/DataSet/txtFinal.txt')))
+def write_csv(counter):
+    with open('C:/Users/USER/Desktop/DS3/PFE/DataSet/output1.csv', 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        header = ['IP', 'FREQUENCY']
+        writer.writerow(header)
+
+        for item in counter:
+            writer.writerow((item, counter[item]))
+
+
+write_csv(count(reader('C:/Users/USER/Desktop/DS3/PFE/DataSet/txtFinal.log')))
 
 
 def parse_str(x):
     """
-    Returns the string delimited by two characters.
-
-    Example:
-        `>>> parse_str('[my string]')`
-        `'my string'`
+    enlever les accollades
     """
     return x[1:-1]
 
 
 def parse_datetime(x):
-    '''
-    Parses datetime with timezone formatted as:
-        `[day/month/year:hour:minute:second zone]`
 
-    Example:
-        `>>> parse_datetime('13/Nov/2015:11:45:42 +0000')`
-        `datetime.datetime(2015, 11, 3, 11, 45, 4, tzinfo=<UTC>)`
-
-    Due to problems parsing the timezone (`%z`) with `datetime.strptime`, the
-    timezone will be obtained using the `pytz` library.
-    '''
     dt = datetime.strptime(x[1:-7], '%d/%b/%Y:%H:%M:%S')
+
     dt_tz = int(x[-6:-3]) * 60 + int(x[-3:-1])
     return dt.replace(tzinfo=pytz.FixedOffset(dt_tz))
 
 
 data = pd.read_csv(
-    'C:/Users/USER/Desktop/DS3/PFE/DataSet/txtFinal.log',
+    'C:/Users/USER/Desktop/DS3/PFE/DataSet/txtFinal.LOG',
     sep=r'\s(?=(?:[^"]*"[^"]*")*[^"]*$)(?![^\[]*\])',
     engine='python',
-    na_values='-',
-    header=None,
-    usecols=[0, 3, 4, 5, 6, 7, 8],
+    na_values='-',  # Nan
+    header=None,  # automatically assign the first row of df (which is the actual column names) to the first row
+    usecols=[0, 3, 4, 5, 6, 7, 8],  # eliminer les 2 tirets qui se trouvent après l'@ IP .
     names=['ip', 'time', 'request', 'status', 'size', 'referer', 'user_agent'],
     converters={'time': parse_datetime,
                 'request': parse_str,
@@ -138,5 +127,42 @@ data = pd.read_csv(
                 'referer': parse_str,
                 'user_agent': parse_str})
 
+data = data.sort_values(by="time")
 
-data.to_csv(r'C:/Users/USER/Desktop/DS3/PFE/DataSet/OUTPUT.csv', index = False)
+data.to_csv(r'C:/Users/USER/Desktop/DS3/PFE/DataSet/OUTPUT.csv', index=False)
+
+# checking missing values
+data.isnull().sum()
+
+# user agent confirmation
+userAgent = DataFrame(data.groupby(['user_agent']).size().index)
+userAgent['count'] = data.groupby(['user_agent']).size().values
+userAgent
+
+# visualisation de  user agent
+plt.figure(figsize=(15, 10))
+plt.pie(userAgent['count'], labels=userAgent['user_agent'], autopct='%1.1f%%', shadow=True, startangle=90)
+plt.title('User Agent')
+plt.show()
+
+
+
+#visualisation du code de réponse
+
+plt.figure(figsize = (10, 10))
+plt.pie(data.groupby([data['status'] // 100]).count().time,  counterclock=False, startangle=90)
+
+labels2 = ['200','206','301','302','304','400','403','404','408']
+plt.pie(data.groupby(['status']).count().time, labels = labels2 ,counterclock=False, startangle=90, radius=0.7)
+
+centre_circle = plt.Circle((0,0),0.4, fc='white')
+fig = plt.gcf()
+fig.gca().add_artist(centre_circle)
+plt.title('Error Status Code')
+plt.show()
+
+
+
+
+
+
